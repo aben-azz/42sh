4bc62a1efe93df604c92253136ce55ec6fb45ccf
Achraf <macadelic1030@gmail.com>
2019-05-27 06:28:15 +0200

    Correction bug autocompletion. TODO: afficher les mots selectionné dans lautocompletion en live

--- srcs/prompt/completion.c
+++ srcs/prompt/completion.c
@@ -6,13 +6,13 @@
 /*   By: aben-azz <aben-azz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/15 06:02:13 by aben-azz          #+#    #+#             */
-/*   Updated: 2019/05/27 05:40:50 by aben-azz         ###   ########.fr       */
+/*   Updated: 2019/05/27 06:24:38 by aben-azz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-int		init_autocomp(t_ab *autocomp)
+int		init_autocomp(t_cap *tcap, t_ab *autocomp)
 {
 	int max_offset;
 
@@ -20,12 +20,13 @@ int		init_autocomp(t_ab *autocomp)
 	autocomp->row = g_shell->tcap->cursx_max / (max(max_offset + 2, 1));
 	autocomp->col = autocomp->len / max(autocomp->row, 1);
 	autocomp->carry = autocomp->len % max(autocomp->row, 1);
+	dprintf(debug(), " avant %s\n", tcap->command);
 	return (max_offset);
 }
 
 int		print_name(t_ab *autocomp, char *str, int i)
 {
-	dprintf(debug(), " euuh xd %d %d\n", i, autocomp->pos);
+
 	if (i == autocomp->pos)
 		ft_putstr("\x1b[7m");
 	ft_putstr(str);
@@ -87,13 +88,14 @@ int		ft_tab(t_cap *tcap, t_ab *autocomp)
 	int row;
 	int col;
 	int i;
-	char *command;
+	//char *command;
 
 	get_words_completion(autocomp, ".");
-	init_autocomp(autocomp);
+	init_autocomp(tcap, autocomp);
 	i = 0;
 	col = -1;
-	ft_move(tcap, "down", 2);
+	tputs(tcap->clr_all_line, 1, ft_put_termcaps);
+	ft_move(tcap, "down", 1);
 	while (++col < autocomp->col)
 	{
 		row = -1;
@@ -107,10 +109,19 @@ int		ft_tab(t_cap *tcap, t_ab *autocomp)
 		while (row--)
 			print_name(autocomp, autocomp->data[i].name, i) && i++;
 	}
-	//ft_replace_cursor(tcap);
-	ft_move(tcap, "up", autocomp->col + (autocomp->carry > 0 ? 1 : +1) + 1);
-	command = ft_strjoin(autocomp->match, g_shell->autocomp->data[g_shell->autocomp->pos].name);
-	ft_clear_replace(tcap);
-	ft_insert(command, tcap);
+	ft_replace_cursor(tcap);
+	i = 0;
+	while (i < autocomp->col + (autocomp->carry > 0 ? 1 : +1))
+	{
+		ft_move(tcap, "up", 1);
+		//tputs(tcap->clr_curr_line, 1, ft_put_termcaps);
+		i++;
+	}
+
+	// g_shell->autocomp->state = 0;
+	// ft_clear_replace(tcap);
+	// tputs(tcap->clr_all_line, 1, ft_put_termcaps);
+	// ft_insert(ft_strjoin(g_shell->autocomp->match, g_shell->autocomp->data[g_shell->autocomp->pos].name), tcap);
+	// g_shell->autocomp->pos = 0;
 	return (1);
 }
